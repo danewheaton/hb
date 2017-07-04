b0995048c16d4271cfc8e40f17df67a5a182179d
danewheaton <wheaton.dane@gmail.com>
2017-07-04 15:27:46 -0500

    started work on automatic zoom

---
+++ Assets/Scripts/ShardZoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShardZoom : MonoBehaviour
+{
+    vp_FPPlayerEventHandler FPPlayer;
+    Transform otherObject = null;
+    bool inTrigger;
+
+    private void Start()
+    {
+        FPPlayer = GetComponent<vp_FPPlayerEventHandler>();
+    }
+
+    private void Update()
+    {
+        RaycastHit[] hits;
+        Ray ray = new Ray(Camera.main.transform.position, transform.forward);
+        hits = Physics.RaycastAll(ray, 10);
+
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.transform.gameObject.tag == "Secret")
+                FPPlayer.Zoom.TryStart();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Secret")
+        {
+            inTrigger = true;
+            otherObject = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+        FPPlayer.Zoom.TryStop();
+    }
+
+    public void StopZooming()
+    {
+        inTrigger = false;
+        FPPlayer.Zoom.TryStop();
+    }
+}
