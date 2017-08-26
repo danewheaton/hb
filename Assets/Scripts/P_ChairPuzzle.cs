65095793c131c4ca352032fd1cd6a62becfe7256
danewheaton <wheaton.dane@gmail.com>
2017-08-26 13:10:31 -0500

    implemented backup plan for food (trigger zones and Vector3.Angle, ala Nathan Drake (again))

--- Assets/Scripts/P_ChairPuzzle.cs
+++ Assets/Scripts/P_ChairPuzzle.cs
@@ -8,49 +8,80 @@ public class P_ChairPuzzle : MonoBehaviour
     [SerializeField] Transform table;
     [SerializeField] float angle = 60;
     [SerializeField] GameObject winObject;
+    [SerializeField] Collider frontTrigger, rearTrigger;
 
     vp_FPPlayerEventHandler FPPlayer;
+    Credits creditsPanel;
 
     float timer;
-    bool solvedPuzzle;
+    bool solvedPuzzle, inTrigger;
 
     private void Start()
     {
         FPPlayer = FindObjectOfType<vp_FPPlayerEventHandler>();
+        creditsPanel = FindObjectOfType<Credits>();
     }
 
     void Update ()
     {
-        if (!solvedPuzzle)
+        if (!solvedPuzzle && inTrigger)
         {
             if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) timer += Time.deltaTime;
             else timer = 0;
 
+            //Vector3 tableDirection = table.position - transform.position;
+
+            //foreach (GameObject g in GameObject.FindGameObjectsWithTag("Chair"))
+            //{
+            //    Renderer chair = g.GetComponent<Renderer>();
+
+            //    if (chair.IsVisibleFrom(Camera.main) &&
+            //        Vector3.Angle(tableDirection, transform.forward) <= angle &&
+            //        FPPlayer.CurrentWeaponName.Get() == "2Lens" &&
+            //        (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
+            //    {
+            //        if (!InvisibleChairs.Contains(chair)) InvisibleChairs.Add(chair);
+
+            //        if (InvisibleChairs.Count == 18)
+            //        {
+            //            if (timer > .5f)
+            //            {
+            //                winObject.SetActive(true);
+            //                solvedPuzzle = true;
+            //            }
+            //        }
+            //    }
+            //    else
+            //        InvisibleChairs.Remove(chair);
+            //}
+
             Vector3 tableDirection = table.position - transform.position;
 
-            foreach (GameObject g in GameObject.FindGameObjectsWithTag("Chair"))
-            {
-                Renderer chair = g.GetComponent<Renderer>();
-
-                if (chair.IsVisibleFrom(Camera.main) &&
-                    Vector3.Angle(tableDirection, transform.forward) <= angle &&
+            if (Vector3.Angle(tableDirection, transform.forward) <= angle &&
                     FPPlayer.CurrentWeaponName.Get() == "2Lens" &&
-                    (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
-                {
-                    if (!InvisibleChairs.Contains(chair)) InvisibleChairs.Add(chair);
-
-                    if (InvisibleChairs.Count == 18)
-                    {
-                        if (timer > .5f)
-                        {
-                            winObject.SetActive(true);
-                            solvedPuzzle = true;
-                        }
-                    }
-                }
-                else
-                    InvisibleChairs.Remove(chair);
+                    (Input.GetMouseButton(0) || Input.GetMouseButton(1)) &&
+                    timer > 2)
+            {
+                winObject.SetActive(true);
+                solvedPuzzle = true;
+                StartCoroutine(creditsPanel.FlashWhite());
             }
         }
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == frontTrigger || other == rearTrigger)
+        {
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == frontTrigger || other == rearTrigger)
+        {
+            inTrigger = false;
+        }
+    }
 }
