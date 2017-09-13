209681f96c3368aa45bb2300e16d0986dd17ed30
danewheaton <wheaton.dane@gmail.com>
2017-09-13 15:51:57 -0500

    finished feast

--- Assets/Scripts/P_ChairPuzzle.cs
+++ Assets/Scripts/P_ChairPuzzle.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class P_ChairPuzzle : MonoBehaviour
 {
     [SerializeField] List<Renderer> InvisibleChairs = new List<Renderer>();
-    [SerializeField] Transform table;
+    [SerializeField] Transform table1, table2;
     [SerializeField] float angle = 60;
     [SerializeField] GameObject winObject, winObject2;
     [SerializeField] Collider frontTrigger, rearTrigger, frontTrigger2, rearTrigger2;
 
     vp_FPPlayerEventHandler FPPlayer;
     Credits creditsPanel;
+    Transform table;
 
     float timer;
-    bool solvedPuzzle, inTrigger;
+    bool solvedPuzzle, inTrigger, inTrigger2;
 
     private void Start()
     {
@@ -24,7 +25,7 @@ public class P_ChairPuzzle : MonoBehaviour
 
     void Update ()
     {
-        if (!solvedPuzzle && inTrigger)
+        if (!solvedPuzzle && (inTrigger || inTrigger2))
         {
             if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) timer += Time.deltaTime;
             else timer = 0;
@@ -46,17 +47,27 @@ public class P_ChairPuzzle : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other == frontTrigger || other == rearTrigger || other == frontTrigger2 || other == rearTrigger2)
+        if (other == frontTrigger || other == rearTrigger)
         {
             inTrigger = true;
+            table = table1;
+        }
+        else if (other == frontTrigger2 || other == rearTrigger2)
+        {
+            inTrigger2 = true;
+            table = table2;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other == frontTrigger || other == rearTrigger || other == frontTrigger2 || other == rearTrigger2)
+        if (other == frontTrigger || other == rearTrigger)
         {
             inTrigger = false;
+        }
+        else if (other == frontTrigger2 || other == rearTrigger2)
+        {
+            inTrigger2 = false;
         }
     }
 }
