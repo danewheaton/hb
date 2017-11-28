fc397c8f9ac9d06c70c3c2abf3075a23b5e25e68
danewheaton <wheaton.dane@gmail.com>
2017-11-27 19:55:35 -0600

    fixed camera hiccup!

---
+++ Assets/Scripts/SmoothMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothMover : MonoBehaviour
+{
+
+    vp_FPController m_Controller = null;
+
+    void Start()
+    {
+        m_Controller = transform.root.GetComponent<vp_FPController>();
+    }
+
+    void LateUpdate()
+    {
+        transform.position = m_Controller.SmoothPosition;
+    }
+
+}
