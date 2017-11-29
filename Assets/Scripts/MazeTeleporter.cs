6b1c58affb0a12d6f7fc5ab811c7c7c1e320c4c5
danewheaton <wheaton.dane@gmail.com>
2017-11-28 23:17:08 -0600

    fixed maze issues

--- Assets/Scripts/MazeTeleporter.cs
+++ Assets/Scripts/MazeTeleporter.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MazeTeleporter : MonoBehaviour
 {
+    [SerializeField] GameObject maze;
     [SerializeField] Transform mazePlayerStart;
 
     private void OnTriggerEnter(Collider other)
     {
+        maze.SetActive(true);
+
         other.transform.position = new Vector3
             (other.transform.position.x, mazePlayerStart.position.y, other.transform.position.z);
     }
