1afa62539087152da33aee71c5ab5abcf92da4a5
Barles Charkley <aidan.a.walsh@gmail.com>
2017-11-30 18:15:21 -0600

    polish, finishing touches, phonebooth adjustment, etc

---
+++ Assets/Scripts/NinetyFive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinetyFive : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] NinetyFiveObjects;
+
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha9) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            for (int i = 0; i < NinetyFiveObjects.Length; i++)
+            {
+                NinetyFiveObjects[i].SetActive(true);
+            }
+        }
+	}
+}
--- Assets/Scripts/NotEdgyCam.cs
+++ Assets/Scripts/NotEdgyCam.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class NotEdgyCam : MonoBehaviour
 {
-	void Update ()
+
+    void Update ()
 	{
 		//transform.position = Camera.main.transform.position;
 		transform.rotation = Camera.main.transform.rotation;
